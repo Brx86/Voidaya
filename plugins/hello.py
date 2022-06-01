2e8089729057a780cd49f2d24dee04648d0797d8
Brx86 <brx86@qq.com>
2022-06-01 10:46:12 +0800

    更改消息类型

--- plugins/hello.py
+++ plugins/hello.py
@@ -1,9 +1,11 @@
-from voidaya import Plugin, text
+from . import logger
+from tools import Message
+from voidaya import Method
 
 
-class PatchPlugin(Plugin):
+class Plugin(Method):
     def match(self):  # 说 hello 则回复
         return self.on_full_match("hello")
 
     async def handle(self):
-        await self.send_msg(text(f"Hello {self.name}"))
+        await self.send_msg(Message.text(f"Hello {self.name}"))
