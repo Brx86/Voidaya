0a091766592a7adf9a9ab7851e72637fc21bc8cb
Brx86 <brx86@qq.com>
2022-06-02 23:10:50 +0800

    使用pathlib的glob方法加载插件

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -13,7 +13,6 @@ import sys, time
 from pathlib import Path
 from loguru import logger
 from config import LOG, LOG_LEVEL
-from pkgutil import iter_modules
 from importlib import import_module
 
 # 使用pathlib获取当前路径
@@ -26,7 +25,7 @@ logger.add(PATH / "logs" / f"{time.strftime('%Y-%m-%d')}.log", enqueue=True)
 
 # 加载插件
 plugin_list = []
-for _, plugin_file, _ in iter_modules(["plugins"]):
+for plugin_path in (PATH / "plugins").glob("[a-z]*.py"):
+    plugin_file = plugin_path.stem
     logger.info(f"Loading plugin: {plugin_file}")
     plugin_list.append(import_module(f"plugins.{plugin_file}"))
-del plugin_file
