274d405f49b695946067ad9dfe0a11c8c02cb4a9
Brx86 <brx86@qq.com>
2022-06-03 22:21:48 +0800

    添加描述与@功能

--- plugins/hello.py
+++ plugins/hello.py
@@ -1,3 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   hello.py
+@Time    :   2022/06/03 21:30:15
+@Author  :   Ayatale 
+@Version :   1.0
+@Contact :   ayatale@qq.com
+@Github  :   https://github.com/Brx86/Voidaya
+@Desc    :   Hello World
+"""
+
+
 from tools import Message
 from voidaya import Method
 
@@ -7,4 +19,7 @@ class Plugin(Method):
         return self.on_full_match("hello")
 
     async def handle(self):
-        await self.send_msg(Message.text(f"Hello {self.name}"))
+        return await self.send_msg(
+            Message.at(self.uid),
+            Message.text(f"Hello {self.name}"),
+        )
